30bad181a0ffa37f970adc75b251f944f9dbb71e
tuyf1 <tuyf@fingard.com>
2019-04-28 14:13:28 +0800

    注解提交

--- src/main/java/com/fingard/ncbc/eureka/EurekaApplication.java
+++ src/main/java/com/fingard/ncbc/eureka/EurekaApplication.java
@@ -4,10 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * 服务器启动类
+ * @author 屠耀锋
+ * @Date 2019-04-25
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaApplication {
 
+    /**
+     * Organization Tree
+     * @param  args 请求参数
+     * @throws Exception
+     */
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class, args);
     }
